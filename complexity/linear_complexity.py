e61250a0953a771070da7a3fde221c213c03eccb
Aleksandr Molchanov <alexandr87m@gmail.com>
2023-06-25 18:33:26 +0500

    feat: Add additional linear algorithm example

--- complexity/linear_complexity.py
+++ complexity/linear_complexity.py
@@ -1,3 +1,5 @@
+import math
+
 def get_minimum(items):
     minimum = items[0]
 
@@ -15,7 +17,21 @@ def get_average(items):
 
     return sum / len(items)
     
+
+def is_prime(number):
+    if number < 2:
+        return False
+
+    sqrt = math.sqrt(number)
+    for i in range(2, int(sqrt)):
+        if number % i == 0:
+            return False
+
+    return True
+
+
 items = [34, 3, 5, 67, 2, 36, 73, 25, 86, 33]
 
 print(get_minimum(items))
 print(get_average(items))
+print(is_prime(7))
