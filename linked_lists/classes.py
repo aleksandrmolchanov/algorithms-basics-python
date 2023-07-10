2c56d75593a531a4ec6532d3da2533148ab2f304
Aleksandr Molchanov <alexandr87m@gmail.com>
2023-07-10 23:08:29 +0500

    feat(linked lists): Add classes

---
+++ linked_lists/classes.py
@@ -0,0 +1,8 @@
+class LinkedListNode:
+    def __init__(self, value, next):
+        self.value = value
+        self.next = next
+    
+class LinkedList:
+    def __init__(self):
+        self.head = None    
